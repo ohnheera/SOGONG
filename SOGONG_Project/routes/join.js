af58f0d69b546fa1db40e45ba172b9e4560f8090
ohnheera <heera.ohn@gmail.com>
2018-06-02 17:15:10 +0900

    commit heera

--- SOGONG_Project/routes/join.js
+++ SOGONG_Project/routes/join.js
@@ -9,7 +9,7 @@ var multer = require('multer');
 var upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './uploads/user/petimg/');
+      cb(null, './uploads-/user/petimg/');
     },
     filename: function (req, file, cb) {
       cb(null, new Date().valueOf() + file.originalname);
