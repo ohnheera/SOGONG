726fddca09ae0612a580ab4c95f563be049964b8
ohnheera <heera.ohn@gmail.com>
2018-06-04 23:16:17 +0900

    장바구니 추가, 메인페이지 정돈함

--- SOGONG_Project/routes/cart.js
+++ SOGONG_Project/routes/cart.js
@@ -18,7 +18,17 @@ router.get('/', function(req, res, next) {
   var id = null;
   var session = req.session;
   var user_id = session.user_id;
-  res.render('cart', { title: 'Cart' , id:user_id});
+  pool.getConnection(function(err,connection)
+  {
+    if(err) console.error("커넥션 객체 얻어오기 에러 : ",err);
+    var sql="select id,product_num,price,amount,name,pic,sale from cart where id=?";
+    connection.query(sql,[id],function(err,row){
+      console.log(row);
+      if(err) console.error("에러 발생 err: ",err);
+				res.render('cart', { title: 'Cart' , row, id:user_id});
+      });
+      connection.release();
+  });
 });
 
 
